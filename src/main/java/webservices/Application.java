2c18bb7a923d009c12e576450877fd4ff9772706
wayz-of-chang <david@dchang.no-ip.org>
2016-05-14 12:12:07 -0500

    Added consumer of the originally-produced RESTful webservice

--- src/main/java/webservices/Application.java
+++ src/main/java/webservices/Application.java
@@ -1,11 +1,24 @@
 package webservices;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        Pong pong = restTemplate.getForObject("http://localhost:8080/ping", Pong.class);
+        log.info(pong.toString());
     }
 }
--- src/main/java/webservices/Ping.java
+++ src/main/java/webservices/Ping.java
@@ -1,5 +1,8 @@
 package webservices;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Ping {
 
     private final long id;
@@ -16,5 +19,13 @@ public class Ping {
 
     public String getContent() {
         return content;
+    }
+
+    @Override
+    public String toString() {
+        return "Ping{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                '}';
     }
 }
