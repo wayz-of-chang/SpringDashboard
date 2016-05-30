1ede3f31d358b8a18509acfcf828e7863a20f45f
wayz-of-chang <david@dchang.no-ip.org>
2016-05-29 22:23:29 -0500

    split out application js into its own file; cleaned up styling of users form

---
+++ src/main/resources/static/js/application.js
@@ -0,0 +1,38 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $('#connect').attr('disabled', connected);
+    $('#disconnect').attr('disabled', !connected);
+    $('#conversationDiv').css('visibility', connected ? 'visible' : 'hidden');
+    $('#response').html('');
+}
+
+function connect() {
+    var socket = new SockJS('/stats');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function(frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/greetings', function(greeting) {
+            showGreeting(JSON.parse(greeting.body).data);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient != null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    var name = $('#name').val();
+    stompClient.send("/app/stats", {}, JSON.stringify({ 'name': name, 'statType': 'system', 'url': 'localhost:8080', 'interval': '1s' }));
+}
+
+function showGreeting(message) {
+    var response = $('#response');
+    response.append('<p style="word-wrap:break-word;">' + message + '</p>');
+}
