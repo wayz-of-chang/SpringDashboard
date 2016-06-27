80b109cdce501358b5a8541f634d050dc545951f
wayz-of-chang <david@dchang.no-ip.org>
2016-06-27 07:44:01 -0500

    added a script client for running arbitrary scripts

---
+++ src/main/java/webservices/client/rest/ScriptClient.java
@@ -0,0 +1,67 @@
+package webservices.client.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import webservices.Message;
+import webservices.data.ScriptData;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+@RestController
+public class ScriptClient extends Client {
+
+    @RequestMapping("/script")
+    public Message script(@RequestParam(value="name", defaultValue="ping.bat") String name) {
+        ExecutorService pool = Executors.newFixedThreadPool(2);
+        int exitValue = -1;
+        ArrayList<String> output = new ArrayList<String>();
+        ArrayList<String> error = new ArrayList<String>();
+        try {
+            ProcessBuilder builder = new ProcessBuilder(getClass().getClassLoader().getResource(String.format("scripts/%s", name)).getPath());
+            Process process = builder.start();
+            Callable<ArrayList<String>> outputCallable = new StreamGobbler(process.getInputStream());
+            Callable<ArrayList<String>> errorCallable = new StreamGobbler(process.getErrorStream());
+            Future<ArrayList<String>> outputFuture = pool.submit(outputCallable);
+            Future<ArrayList<String>> errorFuture = pool.submit(errorCallable);
+            exitValue = process.waitFor();
+            output = outputFuture.get();
+            error = errorFuture.get();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
+        return new Message(counter.incrementAndGet(), new ScriptData(exitValue, output, error), name, new Parameters(name, "script"));
+    }
+}
+
+class StreamGobbler implements Callable<ArrayList<String>> {
+    BufferedReader outputBufferedReader;
+    ArrayList<String> output;
+
+    StreamGobbler(InputStream processOutputStream) {
+        InputStreamReader outputStreamReader = new InputStreamReader(processOutputStream);
+        this.outputBufferedReader = new BufferedReader(outputStreamReader);
+        this.output = new ArrayList<String>();
+    }
+
+    public ArrayList<String> call() {
+        try {
+            String line;
+            while ((line = this.outputBufferedReader.readLine()) != null) {
+                this.output.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return this.output;
+    }
+}
