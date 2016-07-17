5004d08f54e5d6a64aec89c9e78edf822576e9cf
wayz-of-chang <david@dchang.no-ip.org>
2016-07-16 22:33:30 -0500

    streamlined the status indicator while also resolving some bugs and making it look nicer

--- src/main/resources/static/js/statusIndicator.js
+++ src/main/resources/static/js/statusIndicator.js
@@ -5,17 +5,13 @@ function loadStatusIndicator(elementId, value) {
     var width = parseInt(status.style("width"));
     var height = parseInt(status.style("height"));
     status = status.attr("class", "status").attr("width", width).attr("height", height).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    var bars = status.selectAll(".status-indicator").data(value).enter().append("g").attr("class", "status-indicator");
+    var bars = status.append("g").attr("class", "status-indicator");
+    bars.append("rect").attr("class", "status-box").attr("x", 0).attr("y", 0).attr("rx", 20).attr("ry", 20).attr("width", width - margin.left - margin.right).attr("height", function(value) { return height - margin.top - margin.bottom; });
+    bars.append("text").attr("class", "status-label").attr("x", (width - margin.left - margin.right)/2).attr("y", (height - margin.top - margin.bottom) / 1.75).attr("font-size", (Math.round(height - margin.top - margin.bottom) / 3) + "px").attr("text-anchor", "middle").text(value);
 
     function StatusUpdater(){
         this.update = function(value){
-            var bars = status.select(".status-indicator");;
-
-            bars.selectAll("rect").data(value).enter().append("rect").attr("class", "status-box");
-            bars.selectAll("rect").data(value).transition().duration(1000).attr("x", 0).attr("y", 0).attr("width", width - margin.left - margin.right).attr("height", function(value) { return height - margin.top - margin.bottom; });
-
-            bars.selectAll("text").data(value).enter().append("text").attr("class", "status-label");
-            bars.selectAll("text").data(value).text(value).attr("x", (width - margin.left - margin.right)/2).attr("y", (height - margin.top - margin.bottom) / 1.75).attr("font-size", (Math.round(height - margin.top - margin.bottom) / 3) + "px").attr("text-anchor", "middle");
+            status.selectAll("text").text(value);
         }
     }
 
