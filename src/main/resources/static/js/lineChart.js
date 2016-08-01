5553cf7dca4cb4487e713378c57ca344b1c490dd
wayz-of-chang <david@dchang.no-ip.org>
2016-08-01 01:21:17 -0500

    fixed bug with line chart overflowing

--- src/main/resources/static/js/lineChart.js
+++ src/main/resources/static/js/lineChart.js
@@ -20,6 +20,7 @@ function loadLineChart(elementId, values, config) {
     var width = parseInt(chart.style("width"));
     var height = parseInt(chart.style("height"));
     chart = chart.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    chart.append("clipPath").attr("id", "chartBody_" + elementId).append("rect").attr("x", 0).attr("y", 0).attr("width", width - margin.left - margin.right).attr("height", height - margin.top - margin.bottom);
     var x = d3.time.scale().range([0, width - margin.left - margin.right]);
     var xAxis = d3.svg.axis().scale(x).orient("bottom");
     chart.append("g").attr("class", "lineChart x axis").attr("transform", "translate(0," + (height - margin.top - margin.bottom) + ")").call(xAxis);
@@ -33,25 +34,22 @@ function loadLineChart(elementId, values, config) {
 
     function ChartUpdater(){
         this.update = function(dates, values){
-            //x.domain(values.map(function(value) { return value.key; }));
             var x = d3.time.scale().range([0, width - margin.left - margin.right]).domain(d3.extent(dates, function(d) { return d; }));
             var xAxis = d3.svg.axis().scale(x).orient("bottom");
-            chart.selectAll(".lineChart.x.axis").remove();
-            chart.append("g").attr("class", "lineChart x axis").attr("transform", "translate(0," + (height - margin.top - margin.bottom) + ")").call(xAxis);
-            //y.domain([0,  d3.max(values, function(value) { return value.value; })]);
+            chart.select(".lineChart.x.axis").transition().duration(config.transitionTime).call(xAxis);
+
             var y = d3.scale.linear().range([height - margin.top - margin.bottom, 0]).domain([config.minValue, config.maxValue]);
             var yAxis = d3.svg.axis().scale(y).orient("left");
-            chart.selectAll(".lineChart.y.axis").remove();
-            chart.append("g").attr("class", "lineChart y axis").call(yAxis).append("text").attr("transform", "rotate(-90)").attr("y", 6).attr("dy", ".71em").style("text-anchor", "end").text(config.displayUnit);
-            //var z = d3.scale.ordinal().domain(values.map(function(d) { return values.key; }));
+            chart.select(".lineChart.y.axis").transition().duration(config.transitionTime).call(yAxis).select("text").attr("transform", "rotate(-90)").attr("y", 6).attr("dy", ".71em").style("text-anchor", "end").text(config.displayUnit);
+
             var line = d3.svg.line().x(function(d) { return x(d.x); }).y(function(d) { return y(d.y); });
             var lines = chart.selectAll(".lineChart.line").data(values).enter().append("g").attr("class", "lineChart line");
 
             lines.append("path");
-            chart.selectAll("path").data(values).transition().duration(config.transitionTime).attr("d", function(d) { return line(d.values); }).attr("stroke", function(d) { return d.color });
+            chart.selectAll(".lineChart.line path").data(values).transition().duration(config.transitionTime).attr("d", function(d) { return line(d.values); }).attr("stroke", function(d) { return d.color }).attr("clip-path", "url(#chartBody_" + elementId + ")");
 
             lines.append("text").text("");
-            chart.selectAll("text").data(values).transition().duration(config.transitionTime).text(function(d) { return d.key }).attr("fill", function(d) { return d.color; }).attr("text-anchor", "end").attr("transform", function(d) { return "translate(" + x(d.values[d.values.length - 1].x) + "," + y(d.values[d.values.length - 1].y) + ")"; }).attr("text-anchor", "start");
+            chart.selectAll(".lineChart.line text").data(values).transition().duration(config.transitionTime).text(function(d) { return d.key }).attr("fill", function(d) { return d.color; }).attr("text-anchor", "end").attr("transform", function(d) { return "translate(" + x(d.values[d.values.length - 1].x) + "," + y(d.values[d.values.length - 1].y) + ")"; }).attr("text-anchor", "start");
         }
     }
 
